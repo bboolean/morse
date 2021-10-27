8baeb9be1d24e6b26757c6c45d59509925c3eca5
benjamin <benjamin@localhost.localdomain>
2021-10-26 20:16:07 -0400

    Update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject morse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Convert text to morse code."
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot morse.core
